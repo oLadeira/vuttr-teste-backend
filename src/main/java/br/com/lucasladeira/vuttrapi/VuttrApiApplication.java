b99ae8f3811d65937f55855bc5b4a0bd669dfbde
Lucas Ladeira <lucas-ladeira@hotmail.com>
2022-04-11 15:33:30 -0300

    Initial commit

---
+++ src/main/java/br/com/lucasladeira/vuttrapi/VuttrApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.lucasladeira.vuttrapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VuttrApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VuttrApiApplication.class, args);
+	}
+
+}
