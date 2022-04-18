85405b734cd625f61d2ccea12b31a6505b4f79e9
Lucas Ladeira <lucas-ladeira@hotmail.com>
2022-04-18 09:00:49 -0300

    ToolController getAllTools

--- src/main/java/br/com/lucasladeira/vuttrapi/controllers/ToolController.java
+++ src/main/java/br/com/lucasladeira/vuttrapi/controllers/ToolController.java
@@ -2,15 +2,17 @@ package br.com.lucasladeira.vuttrapi.controllers;
 
 import br.com.lucasladeira.vuttrapi.dto.NewToolDto;
 import br.com.lucasladeira.vuttrapi.dto.ToolDto;
+import br.com.lucasladeira.vuttrapi.entities.Tool;
+import br.com.lucasladeira.vuttrapi.repositories.ToolRepository;
 import br.com.lucasladeira.vuttrapi.services.ToolServiceImpl;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/tools")
@@ -26,5 +28,14 @@ public class ToolController {
     @PostMapping
     public ResponseEntity<ToolDto> createTool(@RequestBody NewToolDto tool){
         return ResponseEntity.status(HttpStatus.CREATED).body(mapper.map(toolService.createTool(tool), ToolDto.class));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ToolDto>> getAllTools(){
+        List<ToolDto> tools = toolService.getAllTools()
+                .stream().map(
+                        tool -> mapper.map(tool, ToolDto.class)).collect(Collectors.toList());
+
+        return ResponseEntity.status(HttpStatus.OK).body(tools);
     }
 }
