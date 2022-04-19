021ed53f8a59431d03906b39e6f82f0f8f3882df
Lucas Ladeira <lucas-ladeira@hotmail.com>
2022-04-19 10:18:12 -0300

    test of ToolServiceImpl class getAllToolsByTag method object not found exception

---
+++ src/test/java/br/com/lucasladeira/vuttrapi/services/ToolServiceImplTest.java
@@ -0,0 +1,94 @@
+package br.com.lucasladeira.vuttrapi.services;
+
+import br.com.lucasladeira.vuttrapi.dto.NewToolDto;
+import br.com.lucasladeira.vuttrapi.dto.ToolDto;
+import br.com.lucasladeira.vuttrapi.entities.Tool;
+import br.com.lucasladeira.vuttrapi.repositories.ToolRepository;
+import br.com.lucasladeira.vuttrapi.services.exceptions.ObjectNotFoundException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ToolServiceImplTest {
+
+    //mock data
+    public static final long ID = 100L;
+    public static final String TITLE = "C99297g5FP";
+    public static final String LINK = "9C04yJy9";
+    public static final String DESCRIPTION = "E3ORC7ZNR0SCY0";
+    public static final List<String> TAGS = Arrays.asList("3FHuBo293y", "MqkCyC2y1687");
+
+    @InjectMocks
+    private ToolServiceImpl toolService;
+
+    @Mock
+    private ToolRepository toolRepository;
+
+    @Mock
+    private ModelMapper mapper;
+
+    private Tool tool;
+    private NewToolDto newToolDto;
+    private ToolDto toolDto;
+    private Optional<Tool> optionalTool;
+
+
+    @BeforeEach
+    void setUp(){
+        MockitoAnnotations.openMocks(this);
+        startTool();
+    }
+
+
+    @Test
+    void getAllTools() {
+    }
+
+    @Test
+    void getAllToolsByTag() {
+    }
+
+    @Test
+    void whenFindByTagThenReturnAnObjectNotFoundException(){
+        //estou forçando o repository jogar uma excecao
+        Mockito.when(toolRepository.findByTags(Mockito.anyString())).thenThrow(new ObjectNotFoundException("Tag not found!"));
+
+        try {
+            //vai jogar uma excecao independente do parametro passado
+            toolService.getAllToolsByTag("api");
+        }catch (Exception ex){
+            Assertions.assertEquals(ObjectNotFoundException.class, ex.getClass());
+            Assertions.assertEquals("Tag not found!", ex.getMessage());
+        }
+    }
+
+    @Test
+    void createTool() {
+    }
+
+    @Test
+    void deleteTool() {
+    }
+
+    private void startTool(){
+        tool = new Tool(ID, TITLE, LINK, DESCRIPTION, TAGS);
+        toolDto = new ToolDto(ID, TITLE, LINK, DESCRIPTION, TAGS);
+        newToolDto = new NewToolDto(TITLE, LINK, DESCRIPTION, TAGS);
+        optionalTool = Optional.of(new Tool(ID, TITLE, LINK, DESCRIPTION, TAGS));
+    }
+}
