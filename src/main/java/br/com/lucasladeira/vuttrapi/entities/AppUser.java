6eb60c1d364b7dc7522ca7d817e0e6110fccf9b5
Lucas Ladeira <lucas-ladeira@hotmail.com>
2022-04-20 16:16:14 -0300

    AppUser entity

---
+++ src/main/java/br/com/lucasladeira/vuttrapi/entities/AppUser.java
@@ -0,0 +1,24 @@
+package br.com.lucasladeira.vuttrapi.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "user")
+public class AppUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+    private String password;
+}
