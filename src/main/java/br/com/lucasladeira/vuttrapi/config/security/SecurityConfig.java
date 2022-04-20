ceb0421b5e4a85094bff40646870d9765bd8c5ab
Lucas Ladeira <lucas-ladeira@hotmail.com>
2022-04-20 15:57:59 -0300

    configuring Basic Auth in SecurityConfig class

---
+++ src/main/java/br/com/lucasladeira/vuttrapi/config/security/SecurityConfig.java
@@ -0,0 +1,53 @@
+package br.com.lucasladeira.vuttrapi.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private static final String[] PUBLIC_MATCHERS = {
+            "/h2-console/**"
+    };
+
+    private static final String[] USER_MATCHERS = {
+        "/tools/**"
+    };
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .httpBasic()
+                .and()
+                .authorizeRequests()
+                    .antMatchers(PUBLIC_MATCHERS).permitAll()
+                    .anyRequest().authenticated()
+                .and()
+                .csrf().disable()
+                .cors().disable()
+                .headers().frameOptions().disable()
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("lucas").password(passwordEncoder().encode("321")).roles("USER")
+                .and()
+                .withUser("oLadeira").password(passwordEncoder().encode("123")).roles("ADMIN", "USER");
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
