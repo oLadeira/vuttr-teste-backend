457385f1855fe0278366e240eb758334d2d955a0
Lucas Ladeira <lucas-ladeira@hotmail.com>
2022-04-20 16:22:59 -0300

    AppUser & NewAppUser DTO

---
+++ src/main/java/br/com/lucasladeira/vuttrapi/dto/AppUserDto.java
@@ -0,0 +1,18 @@
+package br.com.lucasladeira.vuttrapi.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AppUserDto {
+
+    private Long id;
+
+    private String username;
+
+}
